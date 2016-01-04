127c9af9f9292a16cc936028f38cdfff3d109338
Ryan Cleere <rcleere@planet.com>
2016-01-04 10:27:27 -0600

    tagging

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
-docker build -t ply:latest -f docker-files/Dockerfile .
+SHORT_COMMIT=$(echo $GIT_COMMIT | awk '{ print substr($1, 0, 10) }')
+
+if [[ ! -z "$SHORT_COMMIT" ]]; then
+    tag=$SHORT_COMMIT
+elif [[ ! -z "$GIT_BRANCH" ]]; then
+    tag=$GIT_BRANCH
+else
+    tag=$(git status -s -b | head -1 | awk '{print $2}')
+fi
+
+echo $tag
+docker build -t ply:${tag} -f docker-files/Dockerfile .
